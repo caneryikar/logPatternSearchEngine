ac89b08db74cbe239e91244bbf443dea5ea85f9b
VAIO <VAIO@192.168.1.31>
2018-03-13 23:34:29 +0300

    readFromFileDraft

--- logPatternSearchEngine/src/com/core/logPatternSearchEngine/Calculation.java
+++ logPatternSearchEngine/src/com/core/logPatternSearchEngine/Calculation.java
@@ -1,10 +1,30 @@
 package com.core.logPatternSearchEngine;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Calculation {
 
 	public int testValue = 2;
+	public String contents;
 	
 	public int returnValue(){
 	return testValue;
 }
+	
+	public String readFromFile() throws FileNotFoundException {
+		
+		Scanner fileIn = new Scanner(new File("C:/test.txt"));
+		
+		
+		//File file = new File("/my/location");
+		contents = fileIn.useDelimiter("\\Z").next();
+		
+		
+		
+		 	return contents;
+		
+	}
+	
 }
