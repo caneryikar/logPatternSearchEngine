efba833df8abc5086394a14c690904497dda4444
ealiyik <ealiyik@TR00107868.ericsson.se>
2018-03-14 16:44:43 +0300

    charCounter

--- logPatternSearchEngine/src/com/core/logPatternSearchEngine/Calculation.java
+++ logPatternSearchEngine/src/com/core/logPatternSearchEngine/Calculation.java
@@ -3,28 +3,55 @@ package com.core.logPatternSearchEngine;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Calculation {
 
 	public int testValue = 2;
-	public String contents;
-	
-	public int returnValue(){
-	return testValue;
-}
-	
-	public String readFromFile() throws FileNotFoundException {
-		
-		Scanner fileIn = new Scanner(new File("C:/test.txt"));
-		
-		
-		//File file = new File("/my/location");
-		contents = fileIn.useDelimiter("\\Z").next();
-		
-		
-		
-		 	return contents;
-		
+	public String characterCount;
+	public String lineCount;
+
+	public int returnValue() {
+		return testValue;
 	}
+
+	public String readFromFile() throws FileNotFoundException {
+
+		Scanner fileIn = new Scanner(new File("C:/test.txt"));
+		Scanner fileIn2 = new Scanner(new File("C:/test.txt"));
+
+		characterCount = fileIn.useDelimiter("\\Z").next();
+//		lineCount = fileIn2.useDelimiter("\r").next();
+
+		
+		
+		fileIn2.useDelimiter("[;\r\n]+");
 	
+		while (fileIn2.hasNext())
+		{
+			if(fileIn2.next()=="\r")
+		  System.out.printf("%s%n", fileIn2.next());
+		}
+		//
+		// while( fileIn2.hasNextLine())
+		// {
+		// lineCount ++ ;
+		// System.out.println;
+		// System.out.println("Line count: "+ lineCount);
+		//
+		// }
+		//
+		// while (lineCount.)
+		// for (String item : sc2.nextLine().split(";"))
+		// {
+		// System.out.printf("%s%n", item);
+		// }
+
+		System.out.println("Character count: " + characterCount.length());
+	//	System.out.println("Line count: " + lineCount.length());
+
+		return characterCount;
+
+	}
+
 }
--- logPatternSearchEngine/src/com/core/logPatternSearchEngine/Main.java
+++ logPatternSearchEngine/src/com/core/logPatternSearchEngine/Main.java
@@ -7,20 +7,19 @@ public class Main {
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 		System.out.println("HelloWorld");
-		System.out.print(returnValueFromAnotherClass());
+
+		// System.out.print(returnValueFromAnotherClass());
 		System.out.print(returnValueFromFile());
 
-		
 	}
-public static int returnValueFromAnotherClass() {
-	Calculation calc = new Calculation();
-	return calc.returnValue();
-}
 
+	public static int returnValueFromAnotherClass() {
+		Calculation calc = new Calculation();
+		return calc.returnValue();
+	}
 
-
-public static String returnValueFromFile() throws FileNotFoundException {
-	Calculation calcFile = new Calculation();
-	return calcFile.readFromFile();
-}
+	public static String returnValueFromFile() throws FileNotFoundException {
+		Calculation calcFile = new Calculation();
+		return calcFile.readFromFile();
+	}
 }
